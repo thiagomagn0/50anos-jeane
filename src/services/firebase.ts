2b20a89c4b81b18937527a7a9c5a2512e8c4e9aa
Thiago Magno <Thiago.magno@phidelis.com.br>
2026-04-23 18:00:35 -0300

    novo commit

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBakaUFoLdV-Z2SRFZfN4KtrxTBDVBiuNA",
-    authDomain: "lista-cha-d2711.firebaseapp.com",
-    projectId: "lista-cha-d2711",
-    storageBucket: "lista-cha-d2711.firebasestorage.app",
-    messagingSenderId: "785457124010",
-    appId: "1:785457124010:web:c9e5f3118079315803b6d3",
-    measurementId: "G-ZE8SGGSFF7"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyA1cTpruPkgmVmP-ExGGrAnuLj7qIWFLvI",
+  authDomain: "anos-d136e.firebaseapp.com",
+  projectId: "anos-d136e",
+  storageBucket: "anos-d136e.firebasestorage.app",
+  messagingSenderId: "665324155759",
+  appId: "1:665324155759:web:fac55e56f796ca06666e9a",
+  measurementId: "G-9531FG71PN"
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
